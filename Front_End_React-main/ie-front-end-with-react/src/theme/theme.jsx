83f9a7ba9fd561fb4f6156d1abbcfd500bde0787
Julian <juli.rivera.quintero@gmail.com>
2024-09-09 15:38:01 -0500

    feat(frontend): Refactor en Theme.jsx

--- Front_End_React-main/ie-front-end-with-react/src/theme/theme.jsx
+++ Front_End_React-main/ie-front-end-with-react/src/theme/theme.jsx
@@ -8,7 +8,52 @@ const theme = createTheme({
       secondary: {
         main: '#007832', // Color secundario
       },
+      error: {
+        main: '#f44336',
+
+      },
+      background: {
+        default: '#ffffff',
+      }
+
+
     },
+    typography: {
+      fontFamily: 'Roboto, sans-serif',
+      h1: {
+        fontSize: '2.5rem',
+      },
+      h2: {
+      fontSize: '2rem',
+      },
+      body1: {
+        fontSize: '1rem',
+      },
+
+    },
+    spacing: 8,
+    shape: {
+      borderRadius: 8,
+    },
+    breakpoints:{
+      values: {
+        xs: 0,
+        sm:600,
+        md: 960,
+        lg:1280,
+        xl: 1920,
+      },
+    },
+    components:{
+      MuiButton:{
+        styleOverrides:{
+          root:{
+            textTransform:'none',
+          }
+        }
+      }
+    }
+
   })
 
   export default theme;
